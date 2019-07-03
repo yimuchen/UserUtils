935f15014524a9309021b64a35974009608dfaa5
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2019-07-03 09:56:07 -0400

    New variadic interface for boost formatting

--- Common/interface/STLUtils/StringUtils.hpp
+++ Common/interface/STLUtils/StringUtils.hpp
@@ -6,6 +6,7 @@
 #ifndef USERUTILS_COMMON_STLUTILS_STRINGPATHUTILS_HPP
 #define USERUTILS_COMMON_STLUTILS_STRINGPATHUTILS_HPP
 
+#include <boost/format.hpp>
 #include <string>
 #include <vector>
 
@@ -46,6 +47,40 @@ extern void StripSubstring( std::string&, const std::string& x );
 
 extern size_t MatchBrace( const std::string&, const unsigned open_brace );
 extern size_t NextOpenBrace( const std::string&, const unsigned start );
+
+
+/**
+ * @{
+ * @brief Variadic interface for generating formmatted strings.
+ *
+ * The use of this function function would be more similar to the printf of C.
+ * With the function returning a std::string instead of printing to a stream.
+ * This is intended to simply boost::format calls were complicated formatting
+ * isn't required. For examples:
+ *
+ * ```c++
+ * std::cout << fstr("%d %d", myint, myunsigned ) << std::endl;
+ * ```
+ */
+inline std::string
+fstr( boost::format& fmt )
+{  return boost::str( fmt ); }
+
+template<typename TYPE,typename ... ARGS>
+inline std::string
+fstr( boost::format& fmt, TYPE&& T,  ARGS&& ... args )
+{
+  return fstr( fmt % std::forward<TYPE>(T), std::forward<ARGS>( args )... );
+}
+
+template<typename ... ARGS>
+inline std::string
+fstr( const std::string& fmt, ARGS&& ... args )
+{
+  boost::format f( fmt );
+  return fstr( f, std::forward<ARGS>(args)... );
+}
+/** @} */
 
 /** @} */
 
