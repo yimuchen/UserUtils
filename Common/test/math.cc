cd5a214dd54bc28a2d2d1c2c930fa33abe7be362
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2020-01-24 12:24:32 -0500

    Unit testing for math related functions

---
+++ Common/test/math.cc
@@ -0,0 +1,12 @@
+#ifdef CMSSW_GIT_HASH
+#include "UserUtils/Common/interface/Maths.hpp"
+#include "UserUtils/Common/interface/Format.hpp"
+#else
+#include "UserUtils/Common/Maths.hpp"
+#include "UserUtils/Common/Format.hpp"
+#endif
+
+int main()
+{
+  return 0;
+}
