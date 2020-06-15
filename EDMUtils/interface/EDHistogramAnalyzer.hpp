f1ba50f1001db62d9b6462bbdef24dbc17cefade
Yi-Mu "Enoch" Chen <yi-mu.chen@cern.ch>
2020-06-15 15:19:03 -0400

    Added 2D histogram capabilities

--- EDMUtils/interface/EDHistogramAnalyzer.hpp
+++ EDMUtils/interface/EDHistogramAnalyzer.hpp
@@ -4,6 +4,9 @@
 
 #include "UserUtils/EDMUtils/interface/PluginAlias.hpp"
 
+#include "TH1D.h"
+#include "TH2D.h"
+
 namespace usr
 {
 
@@ -16,17 +19,34 @@ public:
 protected:
   edm::Service<TFileService> fs;
   std::map<std::string, TH1D*> _histmap;
+  std::map<std::string, TH2D*> _hist2dmap;
 
   template<typename ... Args>
-  inline TH1D& BookHist1D( const std::string& name, Args ... args )
+  inline TH1D&
+  BookHist1D( const std::string& name, Args ... args )
   {
     _histmap[name] = fs->make<TH1D>( name.c_str(), "", args ... );
     return *( _histmap[name] );
   }
 
-  inline TH1D& Hist( const std::string& x )
+  template<typename ... Args>
+  inline TH2D&
+  BookHist2D( const std::string& name, Args ... args )
+  {
+    _hist2dmap[name] = fs->make<TH2D>( name.c_str(), "", args ... );
+    return *( _hist2dmap[name] );
+  }
+
+  inline TH1D&
+  Hist( const std::string& x )
   {
     return *( _histmap[x] );
+  }
+
+  inline TH2D&
+  Hist2D( const std::string& x )
+  {
+    return *( _hist2dmap[x] );
   }
 };
 
