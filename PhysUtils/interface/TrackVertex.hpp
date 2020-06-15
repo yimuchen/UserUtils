293ca6eb7165258757f9757d042b65092bdcddac
Yi-Mu "Enoch" Chen <yi-mu.chen@cern.ch>
2020-06-15 15:45:15 -0400

    Added Track/Vertex calculation functions

---
+++ PhysUtils/interface/TrackVertex.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file ObjectExtendedVars.hpp
+ * @author [Yi-Mu "Enoch" Chen](https://github.com/yimuchen/)
+ * @brief additional calculation of track and vertex related values
+ */
+#ifndef USERUTILS_PHYSUTILS_TRACKVERTEX_HPP
+#define USERUTILS_PHYSUTILS_TRACKVERTEX_HPP
+
+#include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
+
+#include "TLorentzVector.h"
+
+extern TLorentzVector TransientVertexP4( const TransientVertex&  );
+
+
+#endif
---
+++ PhysUtils/src/TrackVertex.cc
@@ -0,0 +1,24 @@
+#include "UserUtils/PhysUtils/interface/TrackVertex.hpp"
+
+/**
+ * @brief Returning the vertex 4 momentum by summing over all associated tracks,
+ *
+ * Assumes the resulting particles are massless!
+ */
+TLorentzVector TransientVertexP4( const TransientVertex& vertex )
+{
+  TLorentzVector ans( 0, 0, 0, 0 );
+
+  for( const auto ttrack : vertex.originalTracks() ){
+    TLorentzVector tvec;
+    tvec.SetPtEtaPhiM( ttrack.track().pt(),
+      ttrack.track().eta(),
+      ttrack.track().phi(),
+      0 );// Asserting the track to be massless for now
+
+    ans += tvec;
+  }
+
+  return ans;
+
+}
