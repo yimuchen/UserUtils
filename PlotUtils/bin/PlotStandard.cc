9b54cf1af92c38e15f61a42f43cfc99d7fa2fc7e
Yi-Mu "Enoch" Chen <yi-mu.chen@cern.ch>
2020-06-16 23:38:39 -0400

    Removed boost property tree stuff

--- PlotUtils/bin/PlotStandard.cc
+++ PlotUtils/bin/PlotStandard.cc
@@ -1,11 +1,9 @@
 #ifdef CMSSW_GIT_HASH
 #include "UserUtils/Common/interface/ArgumentExtender.hpp"
-#include "UserUtils/Common/interface/BoostUtils/PTreeUtils.hpp"
 #include "UserUtils/Common/interface/STLUtils/StringUtils.hpp"
 #include "UserUtils/PlotUtils/interface/StandardPlotFormat.hpp"
 #else
 #include "UserUtils/Common/ArgumentExtender.hpp"
-#include "UserUtils/Common/BoostUtils/PTreeUtils.hpp"
 #include "UserUtils/Common/STLUtils/StringUtils.hpp"
 #include "UserUtils/PlotUtils/StandardPlotFormat.hpp"
 #endif
