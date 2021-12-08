71929d32718e8e72ebf896f541ca5a7485596f93
yimuchen <yimuchen@users.noreply.github.com>
2021-12-08 15:04:55 +0000

    deploy: 7ff46751c803acbf256db078e830453d72d70236

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['large_0',['LARGE',['../classusr_1_1plt_1_1font.html#a394c13dba81f4b7c6c2a87d94249add2',1,'usr::plt::font']]],
-  ['large_1',['Large',['../classusr_1_1plt_1_1font.html#ac8ea497154eec96e02c9a9829da2503b',1,'usr::plt::font']]],
+  ['large_0',['Large',['../classusr_1_1plt_1_1font.html#ac8ea497154eec96e02c9a9829da2503b',1,'usr::plt::font']]],
+  ['large_1',['LARGE',['../classusr_1_1plt_1_1font.html#a394c13dba81f4b7c6c2a87d94249add2',1,'usr::plt::font']]],
   ['large_2',['large',['../classusr_1_1plt_1_1font.html#ac3cd16dffb4356946973532d00f58b88',1,'usr::plt::font']]],
   ['lastplot_3',['LastPlot',['../classusr_1_1plt_1_1RooFrame.html#aacf81698befd036fda1a67f55efc7cee',1,'usr::plt::RooFrame']]],
   ['lazy_4',['Lazy',['../group__StatUtils.html#gaffbd9f0f47f3a7487580643edd0f2bb7',1,'usr::Poisson::Lazy()'],['../CommonDistro_8hpp.html#a90eaee08ef3ce1c2088a4e238c8e7bc8',1,'usr::Efficiency::Lazy()']]],
@@ -10,8 +10,8 @@ var searchData=
   ['line_7',['line',['../group__PlotUtilsConstants.html#ga3bb30df1c30a3d0a6ab5ad0d15780f38',1,'usr::plt::sty']]],
   ['linearvariancenll_8',['LinearVarianceNLL',['../group__StatUtils.html#gab032f76ddbdaeb1673ba7b506ffc7dc4',1,'usr']]],
   ['linecolor_9',['LineColor',['../group__PlotUtilsArgument.html#ga0ca2672da7d0cd8b4aff5c5b11fb0cf1',1,'usr::plt']]],
-  ['lineheight_10',['LineHeight',['../classusr_1_1plt_1_1PadBase.html#a444bbeb5e3ee34fc7f0b115348cd20b4',1,'usr::plt::PadBase']]],
-  ['lineheight_11',['lineheight',['../classusr_1_1plt_1_1font.html#a8fbb0a91e32c0b3c77507d9efdaa84c3',1,'usr::plt::font']]],
+  ['lineheight_10',['lineheight',['../classusr_1_1plt_1_1font.html#a8fbb0a91e32c0b3c77507d9efdaa84c3',1,'usr::plt::font']]],
+  ['lineheight_11',['LineHeight',['../classusr_1_1plt_1_1PadBase.html#a444bbeb5e3ee34fc7f0b115348cd20b4',1,'usr::plt::PadBase']]],
   ['linestyle_12',['LineStyle',['../group__PlotUtilsArgument.html#ga4e4e0f0345091c58d7d491e53c6f9016',1,'usr::plt']]],
   ['linewidth_13',['LineWidth',['../group__PlotUtilsArgument.html#ga75eaa4bd82d210e6b22c930b8fd9c86b',1,'usr::plt']]],
   ['linspace_14',['LinSpace',['../group__STLMaths.html#ga64cdb675f9350b7acb3dd78a114c47e9',1,'usr']]],
