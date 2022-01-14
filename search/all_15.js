8c610af5e6d5398cd887031c33040b22ba18051a
yimuchen <yimuchen@users.noreply.github.com>
2022-01-14 17:40:26 +0000

    deploy: cab0503661442749aed1ed77cfc98ae6e6b1fc68

--- search/all_15.js
+++ search/all_15.js
@@ -3,5 +3,5 @@ var searchData=
   ['vector_20enhancements_0',['Vector enhancements',['../group__VecUtils.html',1,'']]],
   ['vectorutils_2ehpp_1',['VectorUtils.hpp',['../VectorUtils_8hpp.html',1,'']]],
   ['violet_2',['violet',['../group__PlotUtilsConstants.html#ga18ee6aa7a6684dfb269f41936fa86edc',1,'usr::plt::col']]],
-  ['visualizeerror_3',['VisualizeError',['../group__PlotUtilsArgument.html#ga84f7c426ba67387cfb421eb6fe333f8f',1,'usr::plt']]]
+  ['visualizeerror_3',['VisualizeError',['../group__PlotUtilsArgument.html#ga318252bf82dc703ee17ee4dc6a053b36',1,'usr::plt']]]
 ];
--- search/functions_15.js
+++ search/functions_15.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['visualizeerror_0',['VisualizeError',['../group__PlotUtilsArgument.html#ga84f7c426ba67387cfb421eb6fe333f8f',1,'usr::plt']]]
+  ['visualizeerror_0',['VisualizeError',['../group__PlotUtilsArgument.html#ga318252bf82dc703ee17ee4dc6a053b36',1,'usr::plt']]]
 ];
